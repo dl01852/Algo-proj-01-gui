293b6158a4f48e42202e5b51b397c042e2d30c66
DSLewis22 <dl01852@georgiasouthern.edu>
2016-02-23 14:06:03 -0500

    Algo project with Gui

---
+++ Algo-proj-01-GUI/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Algo_proj_01_GUI.Properties;
+
+namespace Algo_proj_01_GUI
+{
+    public partial class Form1 : Form
+    {
+        string[] subArrays = new string[35];
+        private Dictionary<string, string> allData = Utility.getAllData(); 
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < subArrays.Length; i++)
+            {
+                // just to make 2 digit num i.e 01,02,03 instead of 1, 2, 3
+                    subArrays[i] = string.Format("MaxSubArray_{0:00}", i+1); 
+            }
+            comboBox1.DataSource = subArrays;
+
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string paddingIndex = string.Format("{0:00}", comboBox1.SelectedIndex+1);
+            string file = "MaxSubArray_" + paddingIndex;
+            var data =
+                allData[file].Trim()
+                    .Replace("\r\n", "")
+                    .Split(',')
+                    .Where(f => !string.IsNullOrEmpty(f))
+                    .Select(d => Convert.ToInt32(d))
+                    .ToArray();
+            var getAnswer = Utility.FindMaximumSubArray(data, 0, data.Length - 1);
+            MessageBox.Show(outputformat(file, getAnswer));
+
+        }
+
+        public string outputformat(string csvFileName, SubArray s)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("CSCI 5330 Spring 2016\r\n");
+            sb.Append("David Lewis\r\n");
+            sb.Append("900732205\r\n\r\n");
+            sb.Append(string.Format("Running data for file {0}\r\n",csvFileName));
+            sb.Append(s);
+
+            return sb.ToString();
+        }
+    }
+
+   
+    public struct SubArray
+    {
+        public int Low { get; set; }
+        public int High { get; set; }
+        public int Sum { get; set; }
+
+        public override string ToString()
+        {   
+            StringBuilder sb = new StringBuilder();
+            sb.Append(string.Format("{0}= {1}\r\n", "Low", Low));
+            sb.Append(string.Format("{0}= {1}\r\n", "High", High));
+            sb.Append(string.Format("{0}= {1}\r\n", "Max", Sum));
+            return sb.ToString();
+        }
+    }
+}
---
+++ Algo-proj-01-GUI/Utility.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using Algo_proj_01_GUI.Properties;
+
+namespace Algo_proj_01_GUI
+{
+    static class Utility
+    {
+        private static SubArray FindMaxCrossingSubArray(int[] A, int low, int mid, int high)
+        {
+            int left_sum = -100000;
+            int sum = 0;
+            int max_left = mid;
+            SubArray crossingSubArray = new SubArray();
+                // this is the object for if the contigious subArray croses the midpoint.
+            //int[] values = new int[3]; // this will contain the respective low, high, sum values for each side.
+            for (int i = mid; i > low; i--)
+            {
+                sum += A[i];
+                if (sum > left_sum)
+                {
+                    left_sum = sum;
+                    max_left = i;
+                }
+            }
+
+            int right_sum = -1000000;
+            int max_right = (mid + 1);
+            sum = 0;
+            for (int j = mid + 1; j <= high; j++)
+            {
+                sum += A[j];
+                if (sum > right_sum)
+                {
+                    right_sum = sum;
+                    max_right = j;
+                }
+            }
+
+            // return values...
+            crossingSubArray.Low = max_left;
+            crossingSubArray.High = max_right;
+            crossingSubArray.Sum = left_sum + right_sum;
+            return crossingSubArray;
+        }
+
+        public static SubArray FindMaximumSubArray(int[] A, int low, int high)
+        {
+            int mid;
+            SubArray initialSub = new SubArray();
+            //int[] maxSubLow, maxSubHigh, maxCross;
+            if (low == high)
+            {
+                initialSub.Low = low;
+                initialSub.High = high;
+                initialSub.Sum = A[low];
+                return initialSub;
+            }
+            mid = (low + high)/2;
+            var maxSubLow = FindMaximumSubArray(A, low, mid);
+            var maxSubHigh = FindMaximumSubArray(A, mid + 1, high);
+            var maxSubCross = FindMaxCrossingSubArray(A, low, mid, high);
+
+            if (maxSubLow.Sum >= maxSubHigh.Sum && maxSubLow.Sum >= maxSubCross.Sum)
+                return maxSubLow;
+            if (maxSubHigh.Sum >= maxSubLow.Sum && maxSubHigh.Sum >= maxSubCross.Sum)
+                return maxSubHigh;
+
+            return maxSubCross;
+        }
+
+        public static Dictionary<string, string> getAllData()
+        {
+            Dictionary<string, string> resourceData = new Dictionary<string, string>();
+            ResourceSet resourceSet = Resources.ResourceManager.GetResourceSet(CultureInfo.CurrentUICulture, true, true);
+            foreach (DictionaryEntry entry in resourceSet)
+            {
+                var resourceKey = entry.Key;
+                var resourceValue = entry.Value;
+                resourceData.Add(resourceKey.ToString(), resourceValue.ToString());
+            }
+
+            return resourceData;
+        }
+    }
+}
+
